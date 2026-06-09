f3976b62cd1cbd735a01c0f4a37be9c965dc2056
Widarduino <widarogu@yahoo.com>
2026-06-09 15:28:26 -0400

    working interupt

---
+++ 7_EXTI/README.md
@@ -0,0 +1,1 @@
+![image](./interupt.gif)
--- 7_EXTI/src/main.c
+++ 7_EXTI/src/main.c
@@ -1,5 +1,7 @@
 // 7/07/2026
 
+
+// this uses interupts rather than polling for the button press
 #include "stm32f103x6.h"
 #include <GPIO.h>
 #include <EXTI.h>
@@ -13,9 +15,9 @@ int main() {
   Enable_1S_TIMER();
 
   DEBUG_LED_ON();
-  delayS(3);
+  delayS(2);
   DEBUG_LED_OFF();
-  delayS(1);
+  delayS(2);
 
   initEXTI();
 
@@ -26,13 +28,17 @@ int main() {
 }
 
 static void exti2_callback(){
-
+  if ((GPIOC->ODR & GPIO_ODR_ODR13)){ // if LOW
+    GPIOC->BSRR = GPIO_BSRR_BR13; // enable 
+  } else {
+    GPIOC->BSRR = GPIO_BSRR_BS13; // (disable) active low
+  }
 }
 
 void EXTI2_IRQHandler(void){
   if ((EXTI->PR & EXTI_PR_PR2) != 0){
-
+    // if pending
     EXTI->PR |= EXTI_PR_PR2;
-
+    // clear pending bit
     exti2_callback();
   } }
