9433c89293f8aa55abd45641c21cf12b7e7247a7
Widarduino <widarogu@yahoo.com>
2026-06-11 23:48:07 -0400

    RTC 90% implementation

--- 8_RTC/src/Drivers/RTC.c
+++ 8_RTC/src/Drivers/RTC.c
@@ -3,6 +3,7 @@
 
 void initRTC(void){
 	
+	__disable_irq();
 	RCC->APB1ENR |= RCC_APB1ENR_PWREN; 
 	// enable PWR clk
 	// important for allowing rtc clock config
@@ -11,19 +12,40 @@ void initRTC(void){
 	
 	PWR->CR |= PWR_CR_DBP; // disable backup domain protection
 	
+	RCC->BDCR |= RCC_BDCR_BDRST; // resets the clock
+	RCC->BDCR &= ~(RCC_BDCR_BDRST);
 	
-	RCC->CSR |= RCC_CSR_LSION; // enable LSI
-	while (!(RCC->CSR & RCC_CSR_LSION)){} //poll while LSI turns on
+	RCC->BDCR |= RCC_BDCR_LSEON; // enable LSE oscillator
+	while (!(RCC->BDCR & RCC_BDCR_LSERDY)){} //poll while LSE turns on
 
-	RCC->BDCR &= ~(RCC_BDCR_RTCSEL_1 | RCC_BDCR_RTCSEL_0 ); // reset register
-	RCC->BDCR |= RCC_BDCR_RTCSEL_1; // use LSI as RTC clock
+	RCC->BDCR &= ~(RCC_BDCR_RTCSEL); // reset register
+	RCC->BDCR |= RCC_BDCR_RTCSEL_LSE; // use LSE as RTC clock
 	
+	
+
 	RCC->BDCR |= RCC_BDCR_RTCEN; // enable RTC Clock
+	RTC->CRL &= ~(RTC_CRL_RSF);
+	while(!(RTC->CRL & RTC_CRL_RSF)){} // wait for synchro
 	
 	while (!(RTC->CRL & RTC_CRL_RTOFF)){} // poll the RT until its ready
 	RTC->CRL |= RTC_CRL_CNF; // enter config mode
-
-	RTC->CRL &= ~(RTC_CRL_CNF); // exit config mode
 	
+	// this sets the reload point to 5s
+	RTC->PRLH = 0x0002;
+	RTC->PRLL = 0x7FFF;
+	// RTC->CRL &= ~(RTC_CRL_SECF)
+	// the DIV register shows progress towards PRLL
+	
+	RTC->CRL &= ~(RTC_CRL_CNF); // exit config mode
+	while (!(RTC->CRL & RTC_CRL_RTOFF)){} // poll the RT until its ready
+	
+
+	RTC->CRL &= ~(RTC_CRL_SECF); // reset interupt flag incase
+	RTC->CRH |= RTC_CRH_SECIE; // second interupt enabled
+	while (!(RTC->CRL & RTC_CRL_RTOFF)){} // poll the RT until its ready
+
+	NVIC_EnableIRQ(RTC_IRQn);
+
+	__enable_irq();
 
 }
--- 8_RTC/src/main.c
+++ 8_RTC/src/main.c
@@ -1,19 +1,35 @@
 /*
-toggle LED every second using RTC and interupts
+toggle LED every 5 second using RTC and interupts
  */
 
 #include "stm32f103x6.h"
 #include <GPIO.h>
+#include <TIM.h>
 #include <RTC.h>
 #include <stdint.h>
 
 
+static void RTC_CALLBACK(void);
+
 int main() {
 
   EnableGPIO13();
-
+  initRTC();
 
   while (1) {
 
   }
+}
+
+static void RTC_CALLBACK(void){
+  if ((GPIOC->ODR & GPIO_ODR_ODR13)){ // if LOW
+    GPIOC->BSRR = GPIO_BSRR_BR13; // enable 
+  } else {
+    GPIOC->BSRR = GPIO_BSRR_BS13; // (disable) active low
+  }
+}
+
+void RTC_IRQHandler(void){
+	RTC->CRL &= ~(RTC_CRL_SECF);
+  RTC_CALLBACK();
 }
